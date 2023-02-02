caa3e8e0e4b142b64905ff1411d544320ab4610f
Cory <cory.gardner@slu.edu>
2023-02-01 22:53:32 -0600

    Added first code samples

---
+++ code_samples/circle_area.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace circle_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius =");
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine("The area = : " + area);
+            Console.ReadLine();
+        }
+    }
+}
--- code_samples/hello_world.cs
+++ code_samples/hello_world.cs
@@ -1,1 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class hello_world
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
+
 
