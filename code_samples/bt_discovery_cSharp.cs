cf8373fe00f22c1ab83ebebc9e2872e020d862bb
Cory <cory.gardner@slu.edu>
2023-02-15 23:40:50 -0600

    Week 3 blog and BT code example

---
+++ code_samples/bt_discovery_cSharp.cs
@@ -0,0 +1,18 @@
+﻿using InTheHand.Net.Bluetooth;
+
+public class bt_discovery
+{
+    public static void Main(string[] args)
+    {
+        // bt discovery
+        var client = new BluetoothClient();
+        var devices = client.DiscoverDevices();
+
+        // print discovered devices
+        foreach (var device in devices)
+        {
+            Console.WriteLine($"{device.DeviceAddress} ({device.DeviceName})");
+        }
+    }
+}
+
